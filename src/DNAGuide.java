299300360fe701f5850981f9944b7c2e1a528aac
PimJit <67126622+PimJit@users.noreply.github.com>
2020-06-22 13:54:24 -0700

    getting started, trying to check if making gn with multiple nt would work or not

---
+++ src/DNAGuide.java
@@ -0,0 +1,97 @@
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class DNAGuide {
+//
+//
+//    public static void main(String[] args) {
+//        //input
+//        System.out.println("DNA sequence: ");
+//        Scanner scan = new Scanner(System.in);
+//        String sequence = scan.nextLine();
+//        sequence = sequence.toLowerCase();
+//
+//        //method
+//        //dealing with gn
+//        String comb = "";
+//        ArrayList<String> guideTarget = new ArrayList<>();
+//        ArrayList<String> actualTarget = new ArrayList<>();
+//        for (int a = 11; a <= (sequence.length()-11); a++) {
+//            if (sequence.charAt(a) == 't') {
+//                comb = sequence.substring(a - 11, a + 12);
+//                int number = 0;
+//                for (int b = 17; b < comb.length(); b++) {
+//                    if (comb.charAt(b) == 'g' || comb.charAt(b) == 'c') {
+//                        number += 1;
+//                    }
+//                }
+//                if (number >= 4 && number <= 11) {
+//                    guideTarget.add(comb.substring(0,19));
+//                    actualTarget.add(comb.substring(18));
+//                }
+//            }
+//        }
+//
+//        //loop for targeted sequence
+//
+//        for (int a = 0; a < guideTarget.size(); a++) {
+//            if (guideTarget.get(a).charAt(0) == 't') {
+//                if (guideTarget.get(a).charAt(11) == 'g' || guideTarget.get(a).charAt(11) == 'c') {
+//                    guide18.add(guideTarget.get(a).substring(0,19));
+//                    target.add(actualTarget.get(a));
+//                }
+//            }
+//        }
+//
+//        String sequenceTarget = "";
+//        for (int a = 18; a <= (sequence.length()-18); a++) {
+//            if (sequence.charAt(a) == 'a') {
+//                sequenceTarget = sequence.substring(a - 18, a + 19);
+//                int number = 0;
+//                for (int b = 17; b < sequenceTarget.length(); b++) {
+//                    if (sequenceTarget.charAt(b) == 'g' || sequenceTarget.charAt(b) == 'c') {
+//                        number += 1;
+//                    }
+//                }
+//                if (number >= 4 && number <= 11) {
+//                    guideTarget.add(sequenceTarget.substring(0,19));
+//                    actualTarget.add(sequenceTarget.substring(18));
+//                }
+//            }
+//        }
+//
+//        //loop for targeted sequence
+//        ArrayList<String> guide18 = new ArrayList<>();
+//        ArrayList<String> target = new ArrayList<>();
+//        for (int a = 0; a < guideTarget.size(); a++) {
+//            if (guideTarget.get(a).charAt(0) == 't') {
+//                if (guideTarget.get(a).charAt(11) == 'g' || guideTarget.get(a).charAt(11) == 'c') {
+//                    guide18.add(guideTarget.get(a).substring(0,19));
+//                    target.add(actualTarget.get(a));
+//                }
+//            }
+//        }
+//
+//
+//
+//
+//
+//        //position 1 is T
+//
+//        //position 1 complement to target's position 1 (a)
+//
+//        //12th position is G or C no A
+//
+//        //G/C ratio content is 20-65%
+//
+//        //guide is 16-18 nt
+//
+//        //target is 18-20 long
+//
+////        //output
+//        System.out.println("Targeted sequence: " + target);
+//        System.out.println("DNA guide 18nt");
+//        System.out.println("DNA Guide: " + guide18);
+//
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static String sequence;
+    public static ArrayList<String> totalSequence = new ArrayList<>();        //variable for entire DNA sequence that includes all guides
+
+
+    public static String GuideMaker(int numberNT) {
+        ArrayList<String> gn = new ArrayList<>();
+        for (int a = 11; a <= (sequence.length()-11); a++ ) {
+            if (sequence.charAt(a) == 't') {        //checking for 1st position to be t
+                String potientalGn = sequence.substring(11, numberNT);        //variable for nt length
+                int number = 0;
+                for (int b = 0; b < potientalGn.length(); b++) {
+                    if (potientalGn.charAt(b) == 'g' || potientalGn.charAt(b) == 'c') {     //checking g or c
+                        number += 1;
+                    }
+                }
+                if (number >= 2 && number <= 6) {          //checking g/c content
+                    //totalSequence.add(sequence.substring(0,27));
+                    gn.add(potientalGn);
+                }
+            }
+        }
+
+
+
+
+        return gn + " \n" + totalSequence;
+    }
+
+//    public String GnMaker(int numberNT) {
+//
+//    }
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        System.out.println("DNA sequence: ");
+        Scanner scan = new Scanner(System.in);
+        sequence = scan.nextLine().toLowerCase();
+
+        GuideMaker(16);
+        GuideMaker(17);
+        GuideMaker(18);
+
+
+        //getting gn
+        //setting for loop to begin at 11 so that we wont get be out of bound to make the guide
+
+
+        }
 }
