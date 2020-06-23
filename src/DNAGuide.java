691deb57344b435bd5fa09c1b7a734ac1bdd571d
PimJit <67126622+PimJit@users.noreply.github.com>
2020-06-23 13:29:19 -0700

    Summary: initial code for gt, gr, and gr (without complementary) Refactor:     - Add comment on some variables and some steps for readability Feature:     - Add the initial code for making gt, gf, and gr

--- src/DNAGuide.java
+++ src/DNAGuide.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-
+//Commit
+//need fixing: duplicate of same arraylist when initialized outside method
 public class DNAGuide {
     public static String sequence;
-    public static ArrayList<String> totalSequence = new ArrayList<>();        //variable for entire DNA sequence that includes all guides
+//    public static ArrayList<String> gn16 = new ArrayList<>();       //variable for actual gn with 16 nt
+//    public static ArrayList<String> gn17 = new ArrayList<>();
+//    public static ArrayList<String> gn18 = new ArrayList<>();
 
 
     public static int startGt;
@@ -16,16 +18,29 @@ public class DNAGuide {
     public static int endGr;
 
     public static String guideMaker(int numberNT) {
-        ArrayList<String> gn = new ArrayList<>();       //variable for actual gn
-        ArrayList<String> potentalGn = new ArrayList<>();       //variable for potientalgn to check
-        ArrayList<String> potentalSequence = new ArrayList<>();
-        float numberGC;
-        StringBuilder print1 = new StringBuilder();
+        ArrayList<String> totalSequence = new ArrayList<>();        //variable for entire DNA sequence that includes all guides
 
+        ArrayList<String> gn = new ArrayList<>();       //variable for actual gn
+        ArrayList<String> potentalGn = new ArrayList<>();       //variable for potential to check
+        ArrayList<String> potentalSequence = new ArrayList<>();     //variable for potential full sequence for later checking
+        float numberGC;
+        StringBuilder print1 = new StringBuilder();         //print for gn
+        StringBuilder print2 = new StringBuilder();         //print for totalSequence
+
+
+        ArrayList<String> gf = new ArrayList<>();        //variable for passed gf
+        ArrayList<String> gt = new ArrayList<>();        //variable for passed gt
+        ArrayList<String> gr = new ArrayList<>();        //variable for passed gr
+
+        Boolean potentialGf = false;        //initializing potential gf guides for later check
+        Boolean potentialGt = false;        //initializing potential gt guides for later check
+        Boolean potentialGr = false;        //initializing potential gr guides for later check
+
+        //creating gn
         for (int a = 9; a <= sequence.length()- (10 + numberNT); a++ ) {
             if (sequence.charAt(a) == 't' && sequence.charAt(a + 11) == 'a') {        //checking for 1st position to be t
                 potentalSequence.add(sequence.substring(a - 10, a + (10 + numberNT)));        //variable for nt length
-                potentalGn.add(sequence.substring(a, a + numberNT));
+                potentalGn.add(sequence.substring(a, a + numberNT));        //getting potentalGn sequence for later checking
             }
         }
 
@@ -37,22 +52,63 @@ public class DNAGuide {
                 }
             }
 
-            numberGC = ((float) number)/numberNT;
+            numberGC = ((float) number)/numberNT;       //getting percentage of G/C
             if ( numberGC <= 0.37 && numberGC >= 0.125) {          //checking g/c content
                 totalSequence.add(potentalSequence.get(b));
-                gn.add(potentalGn.get(b) + "   " + numberGC);
-                number = 0;
-                numberGC = 0;
+                gn.add(potentalGn.get(b) + "   " + numberGC);       //getting the passed gn and its G/C percentage
+                number = 0;     //resetting
+                numberGC = 0;   //resetting
+            }
+        }
+
+        //TODO make gt,gf today
+        //creating gt
+        for (int a = 0; a < totalSequence.size(); a++) {
+            //checking gt
+            if (totalSequence.get(a).charAt(0) == 't') {
+                if (totalSequence.get(a).charAt(11) == 'g' || totalSequence.get(a).charAt(0) == 'c') {
+                    potentialGt = true;
+                }
+            }
+
+            //checking gf
+            if (totalSequence.get(a).charAt(10 + (numberNT - 10)) == 't') {
+                if (totalSequence.get(a).charAt(10 + (numberNT + 2)) == 'g' || totalSequence.get(a).charAt(10 + (numberNT + 2)) == 'c') {
+                    potentialGf = true;
+                }
+            }
+
+            //checking gr
+            if (totalSequence.get(a).charAt(20 + numberNT) == 'a') {
+                if (totalSequence.get(a).charAt(10 + (numberNT - 2)) == 'g' || totalSequence.get(a).charAt(10 + (numberNT - 2)) == 'c') {
+                    potentialGr = true;
+                }
+            }
+
+            if (potentialGt && potentialGf && potentialGr) {
+                gt.add(totalSequence.get(a).substring(0,17));       //gt size of 18 nt(?)
+                gf.add(totalSequence.get(a).substring(10 + (numberNT - 10), 29 + (numberNT - 10)));     //gf size of 18 nt(?)
+                gt.add(totalSequence.get(a).substring(20 + numberNT, 29 + (numberNT - 2)));             //gr size of 18 nt(?)
             }
         }
 
 
-        for (int i = 0; i < gn.size();i++) {
+        //return values
+
+        for (int i = 0; i < gn.size(); i++) {            //to print ArrayList
             print1.append(gn.get(i)).append("   ");
         }
 
-        return print1.toString();
+        for (int i = 0; i < totalSequence.size(); i++) {            //to print ArrayList
+            print1.append(totalSequence.get(i)).append("   ");
+        }
+
+        return print1.toString() + "\n" + print2.toString();       //return value from this method
     }
+
+//    public static String guideMaker(int numberNT) {
+//
+//    }
 
 
     public static void main(String[] args) {
